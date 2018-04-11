e92297194a93477242a35680c7b8699779b93524
luisrausseo <30789120+luisrausseo@users.noreply.github.com>
2018-04-11 01:12:14 -0500

    Added Settings Window

---
+++ Assets/InGameGUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameGUI : MonoBehaviour {
+
+    public GameObject MenuGUI;
+
+    public void showWindow()
+    {
+        MenuGUI.SetActive(true);
+    }
+
+    public void closeWindow()
+    {
+        MenuGUI.SetActive(false);
+    }
+
+}
